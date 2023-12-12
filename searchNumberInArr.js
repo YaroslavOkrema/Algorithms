b1f8687390daed7b81f59c98c9cf16be260409f7
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2023-12-12 23:36:57 +0200

    search number in array

---
+++ searchNumberInArr.js
@@ -0,0 +1,13 @@
+// Алгоритм линейного поиска в массиве
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//O(n) линейный
+function searchNumber(arr, number) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === number) {
+            return i;
+        }
+    }
+    return false;
+}
+console.log(searchNumber(arr, 4));
