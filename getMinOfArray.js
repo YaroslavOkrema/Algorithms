b64e31a6fcf697d7f007ea64b3f5741461bbf992
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2023-12-28 23:38:54 +0200

    add second solution

--- getMinOfArray.js
+++ getMinOfArray.js
@@ -2,4 +2,14 @@
 function getMinOfArray(nums) {
     return Math.min(...nums);
 }
-console.log(getMinOfArray([3, 0, 10, 4, 7, 8]));
+console.log(getMinOfArray([3, 0, 10, 4, 7, 8, -1, -10, 20, 22]));
+
+//second solution
+function getMinOfArray(nums) {
+    let currentMin = nums[0];
+    for(let i = 1; i < nums.length; i++) {
+        if(nums[i] < currentMin) currentMin = nums[i];
+    }
+    return currentMin;
+}
+console.log(getMinOfArray([3, 0, 10, 4, 7, 8, -1, -10, 20, 22]));
