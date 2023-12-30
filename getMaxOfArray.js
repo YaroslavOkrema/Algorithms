cc98d7ac8d60cf3768cf91cef0ec76a642fa649a
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2023-12-30 22:05:00 +0200

    second solutin get max of arr

--- getMaxOfArray.js
+++ getMaxOfArray.js
@@ -3,3 +3,13 @@ function getMaxOfArray(nums) {
     return Math.max(...nums);
 }
 console.log(getMaxOfArray([1, 3, 6, 2, 10, 23]));
+
+//second solution
+function getMaxOfArray(arr) {
+    let currentMax = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] > currentMax) currentMax = arr[i];
+    }
+    return currentMax;
+}
+console.log(getMaxOfArray([1, 3, 6, 2, 10, 23, 12, 24, 42, 100]));
