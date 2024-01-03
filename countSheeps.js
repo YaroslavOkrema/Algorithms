87ca8b14686e86d978d99a742dd5e64c855823e2
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-01-04 00:26:46 +0200

    countSheeps

---
+++ countSheeps.js
@@ -0,0 +1,35 @@
+/* 
+Рассмотрим массив/список овец, в котором некоторые овцы могут отсутствовать на своем месте. 
+Нам нужна функция, которая подсчитывает количество овец, присутствующих в массиве (true означает присутствие).
+Например,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true
+]
+
+Правильным ответом будет 17.
+
+Подсказка: не забудьте проверить на наличие плохих значений, таких как null/undefined
+*/
+sheep = [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true
+  ]
+
+function countSheeps(sheep) {
+    let res = 0;
+    for(let i = 0; i < sheep.length; i++) {
+        if(sheep[i] === true) {
+            res++;
+        }
+    }
+    return res;
+}
+console.log(countSheeps(sheep)); // 17
