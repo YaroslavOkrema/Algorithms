1cd61c6782359bbd956b017d439ec39d3bc437d6
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-01-03 22:39:35 +0200

    second solution

--- reverseStr.js
+++ reverseStr.js
@@ -3,3 +3,13 @@ function reverseStr(str) {
     return str.split('').reverse().join('');
 }
 console.log(reverseStr('Hello world'));
+
+// second solution
+function reverseStr2(str) {
+    let newStr = '';
+    for(let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
+}
+console.log(reverseStr('level'));
