440012d7c01840a0c3ac746aa2a0101f2602362d
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-01-14 02:32:47 +0200

    add second solution

--- sumArray.js
+++ sumArray.js
@@ -4,3 +4,13 @@ function sumArray(nums) {
     return nums.reduce((sum, current) => sum + current, 0);
 }
 console.log(sumArray([1, 2, 3, 4, 5]));
+
+// second solution
+function sumArray2(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+console.log(sumArray2([1, 2, 3, 4, 5]));
