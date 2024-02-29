ccb63e58f6e199dd6d5b9aa60c8a3f5e055d2c52
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-02-29 21:56:10 +0200

    filterNumbersAboveThreshold

---
+++ tasks/filterNumbersAboveThreshold.js
@@ -0,0 +1,12 @@
+// Входной массив
+let num = [5, 10, 15, 20, 25, 30];
+
+// Пороговое значение
+//const threshold = 15;
+
+// Ожидаемый результат: [20, 25, 30]
+let filteredNumbers = num.filter(function(currentValue) {
+    return currentValue > 15;
+});
+
+console.log(filteredNumbers); 
