bbebc1d686426784e630dfa1a7934d32736ecdfc
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-03-05 23:09:36 +0200

    feat: classes

--- practice/this/this.js
+++ practice/this/this.js
@@ -1,28 +1,10 @@
-function hello() {
-    console.log('Hello', this);
+//exports.default = {
+//    title: "Phone"
+//}
+
+//console.log(this);
+
+function myFunction() {
+    console.log(this);
 }
-
-const person = {
-    name: 'Yaroslav',
-    age: 25,
-    sayHello: hello,
-    sayHelloWindow: hello.bind(document),
-    logInfo: function(job, phone) {
-        console.group(`${this.name} info:`);
-        console.log(`Name is ${this.name}`);
-        console.log(`Age is ${this.age}`);
-        console.log(`Job is ${job}`);
-        console.log(`Phone is ${phone}`);
-        console.groupEnd();
-    }
-}
-
-console.log(person.logInfo());
-
-const lena = {
-    name: 'Elena',
-    age: 23
-}
-
-person.logInfo.bind(lena, 'Frontend', '21312312312');
-person.logInfo.call(lena, 'Frontend', '21312312312');
+myFunction();
--- tasks/filterNumbersAboveThreshold.js
+++ tasks/filterNumbersAboveThreshold.js
@@ -5,6 +5,7 @@ let num = [5, 10, 15, 20, 25, 30];
 //const threshold = 15;
 
 // Ожидаемый результат: [20, 25, 30]
+
 let filteredNumbers = num.filter(function(currentValue) {
     return currentValue > 15;
 });
