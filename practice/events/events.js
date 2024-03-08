6097e635e52eb293e5463cbfaefce04792a17bc2
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-03-09 01:41:04 +0200

    feat: removeEventListener and options

--- practice/events/events.js
+++ practice/events/events.js
@@ -11,15 +11,30 @@ button.onclick = function () {
 
 // onClick не можно повесить несколько обработчиков на одно и тоже событие
 
-button.addEventListener('click', function () {
+/*button.addEventListener('click', function () {
     console.log('Клик!');
 });
 
 button.addEventListener('click', function () {
     console.log('Клак!');
-})
+})*/
+
+/*function showConsole() {
+    console.log('Квак!');
+    button.removeEventListener('click', showConsole);
+}
+
+button.addEventListener('click', showConsole);*/
+
+const options = {
+    'capture': false, // фаза на которой должен сработать обработчик
+    'once': true, // если тру, то обработчик будет удален после выполнения
+    'passive': false // не вызывает preventDefault()
+}
 
 function showConsole() {
     console.log('Квак!');
 }
+
+button.addEventListener('click', showConsole, options);
 
