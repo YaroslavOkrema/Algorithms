509543f0e947db9926554bc522700c7b3d53a6c3
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-03-10 18:40:09 +0200

    feat: task with classes

--- practice/classes/classes.js
+++ practice/classes/classes.js
@@ -153,7 +153,7 @@ const tesla = new Car('Tesla', 'silver');
 const nissan = new Car('Nissan', 'red');
 console.log();*/
 
-class Car {
+/*class Car {
     constructor(brand) {
         this.brand = brand;
         this.gasTank = 100;
@@ -187,4 +187,42 @@ class HybridCar extends Car {
 
 const lexus = new HybridCar('Lexus', 'RX')
 const nissan = new Car('Nissan')
-console.log(lexus.autoPark());
+console.log(lexus.autoPark());*/
+
+class Clock {
+    constructor( { template } ) {
+        this.template = template;
+    }
+
+    render() {
+        let date = new Date();
+
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+
+        console.log(output);
+    }
+
+    stop() {
+        clearInterval(this.timer);
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render, 1000);
+    }
+}
+
+const clock = new Clock({template: 'h:m:s'});
+clock.start();
