0799e62b9df2ea978172a7b2090305e6e70e5508
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-03-11 19:31:52 +0200

    feat: practice with this

--- practice/this/this.js
+++ practice/this/this.js
@@ -1,10 +1,60 @@
-//exports.default = {
-//    title: "Phone"
-//}
-
-//console.log(this);
-
-function myFunction() {
+/*function getThis() {
     console.log(this);
 }
-myFunction();
+getThis();*/
+
+function getPrice(currency = '$') {
+    console.log(currency + this.price);
+    return this;
+}
+
+function getName() {
+    console.log(this.name);
+    return this;
+}
+
+const prod1 = {
+    name: 'Intel',
+    price: 100,
+    getPrice,
+    getName,
+    info: {
+        info: ['2.3ghz'],
+        getInfo: function () {
+            console.log(this)
+        }
+    }
+}
+/*prod1.getPrice();
+prod1.info.getInfo();
+prod1.getName();*/
+
+const prod2 = {
+    name: 'AMD',
+    price: 50,
+    getPrice
+}
+prod2.getName = prod1.getName;
+prod2.getName();
+// prod2.getPrice();
+
+let str = 'Hello world';
+const reversStr = str.split('').reverse().join('');
+console.log(reversStr);
+
+const prod3 = {
+    name: 'Bmw',
+    price: 2000,
+    getPrice,
+    // getName
+};
+
+getPrice.call(prod3, "*");
+
+getPrice.apply(prod3, ['*']);
+/*
+prod3.getName().getPrice();*/
+
+const getPriceBind = prod3.getPrice.bind(prod3, '*');
+console.log(getPriceBind);
+setTimeout(getPriceBind, 1000);
