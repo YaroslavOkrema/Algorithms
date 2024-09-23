b3f21e36e7597fe39baa57155868e2d7cb6dc1ca
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-09-24 01:20:55 +0300

    feat: mix arr sum

--- tasks/sumArray.js
+++ tasks/sumArray.js
@@ -14,3 +14,20 @@ function sumArray2(arr) {
     return sum;
 }
 console.log(sumArray2([1, 2, 3, 4, 5]));
+
+// Напишите функцию, которая принимает массив строк, чисел и возвращает их сумму
+function mixSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += parseInt(arr[i]);
+    }
+    return sum;
+}
+console.log(mixSum([1, 2, 3, 4, 5, '10']));
+
+function mixSum2(arr) {
+    return arr.reduce(function (sum, num) {
+        return sum + parseInt(num);
+    })
+}
+console.log(mixSum2([1, 2, 3, 4, 5, '10']));
