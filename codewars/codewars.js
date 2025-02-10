ea4d9219ff1eb736bebf1e608c7223663e5ed01e
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-02-10 18:12:25 +0200

    feat: codewars

---
+++ codewars/codewars.js
@@ -0,0 +1,57 @@
+//8 kyu tasks
+
+// Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+//
+// For example, a multiplication table (string) for number == 5 looks like below:
+//
+// 1 * 5 = 5
+// 2 * 5 = 10
+// 3 * 5 = 15
+// 4 * 5 = 20
+// 5 * 5 = 25
+// 6 * 5 = 30
+// 7 * 5 = 35
+// 8 * 5 = 40
+// 9 * 5 = 45
+// 10 * 5 = 50
+
+function multiTable(number) {
+    let table = '';
+
+    for (let i = 1; i <= 10; i++) {
+        table += `${i} * ${number} = ${i * number}\n`;
+    }
+
+    return table;
+}
+
+// console.log(multiTable(5));
+
+
+////////////////////////////////////////////////////////////////
+
+// Знайти суму всіх чисел, кратних n, але менших за m
+// Майте на увазі
+// n та m - натуральні числа (натуральні натуральні числа)
+// m виключено з кратних чисел
+// Приклади:
+// sumMul(2, 9) ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13) ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7) ==> «INVALID»
+
+function sumMul(n,m){
+    let sum = 0;
+
+    if (m <= n) return 'INVALID';
+
+    for (let i = n; i < m; i++) {
+        if (i % n === 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+console.log(sumMul(2, 9));
