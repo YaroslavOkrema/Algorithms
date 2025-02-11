dada1dea7b6ccec4f8ebbdcc5510c98f3d295f85
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-02-11 21:57:54 +0200

    feat: codewars

--- codewars/codewars.js
+++ codewars/codewars.js
@@ -54,4 +54,56 @@ function sumMul(n,m){
     return sum;
 }
 
-console.log(sumMul(2, 9));
+// console.log(sumMul(2, 9));
+
+///////////////////////////////////////////////////////////////////////
+//Напиши стрілкову функцію, яка приймає масив чисел та перетворює його у рядок, використовуючи ASCII-коди.
+//arrowFunc([72, 101, 108, 108, 111]) // "Hello"
+
+const arrowFunction = (arr) => {
+    return arr.map(num => String.fromCharCode(num)).join('');
+}
+
+const arrowFunc = (arr) => {
+    let str = '';
+
+    for (let i = 0; i < arr.length; i++) {
+        str += String.fromCharCode(arr[i]);
+    }
+
+    return str;
+}
+
+// console.log(arrowFunction([70,85,83,32,82,79,72,32,68,65,72]));
+// console.log(arrowFunc([70,85,83,32,82,79,72,32,68,65,72]));
+
+///////////////////////////////////////////////////////////////////////////////////
+//Дано два масиви чисел a і b (довжина ≥ 1). Потрібно перевірити, чи сума квадратів елементів a строго більша за суму кубів елементів b.
+function arrayMadness(a, b) {
+    let arrA = a.map(numbers => numbers * numbers);
+    let arrB = b.map(numbers => numbers * numbers * numbers);
+
+    let sumArrA = arrA.reduce(function(value, acum) {
+        return value + acum;
+    })
+
+    let sumArrB = arrB.reduce(function(value, acum) {
+        return value + acum;
+    })
+
+    if (sumArrA > sumArrB) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function arrMadness2(a, b) {
+    let arrOne = a.reduce((sum, number) => sum + Math.pow(number, 2), 0);
+    let arrTwo = b.reduce((sum, number) => sum + Math.pow(number, 3), 0);
+
+    return arrOne > arrTwo;
+}
+
+console.log(arrayMadness([5,6,7],[4,5,6]));
+console.log(arrMadness2([5,6,7],[4,5,6]));
