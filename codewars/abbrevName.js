7a07aa8da8200bdbd5c1dbe2a718484241b70810
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-02-18 23:55:42 +0200

    feat: codewars

--- codewars/abbrevName.js
+++ codewars/abbrevName.js
@@ -1,6 +1,9 @@
 // "sam harris" => S.H
 function abbrevName(name){
-    return name.split(' ').map(letter => letter[0]).join('.').toUpperCase();
+    return name.split(' ')
+        .map(letter => letter[0])
+        .join('.')
+        .toUpperCase();
 }
 
 console.log(abbrevName("sam harris"));
