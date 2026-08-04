6c7a3eca8e3b0788e6f1a254f9ae5886078dda8f
Yaroslav <yaroslav.okrema@gmail.com>
2026-08-04 21:10:25 +0300

    sumNegativeBetweenMinAndMax

---
+++ training/sumNegativeBetweenMinAndMax.js
@@ -0,0 +1,34 @@
+function sumNegativeBetweenMinAndMax(numbers) {
+    if (numbers.length < 2) return 0;
+
+    let minNumber = numbers[0];
+    let maxNumber = numbers[0];
+    let indexMinNumber = 0;
+    let indexMaxNumber = 0;
+    let sum = 0;
+
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] < minNumber) {
+            minNumber = numbers[i];
+            indexMinNumber = i;
+        }
+
+        if (numbers[i] > maxNumber) {
+            maxNumber = numbers[i];
+            indexMaxNumber = i;
+        }
+    }
+
+    let minIndex = Math.min(indexMinNumber, indexMaxNumber) + 1;
+    let maxIndex = Math.max(indexMinNumber, indexMaxNumber);
+
+    for (let i = minIndex; i < maxIndex; i++) {
+        if (numbers[i] < 0) {
+            sum += numbers[i];
+        }
+    }
+
+    return sum;
+}
+
+console.log(sumNegativeBetweenMinAndMax([3, 8, -2, 5, -1, -5]))
